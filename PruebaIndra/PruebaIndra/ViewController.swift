ab8b66d5ee581d8dae185995482df44072b19ee1
Ever Capuñay Malaver <ecapunay@everis.com>
2021-01-27 06:19:26 -0500

    Se inicio el proyecto.

---
+++ PruebaIndra/PruebaIndra/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  PruebaIndra
+//
+//  Created by Ever Capuñay on 1/27/21.
+//  Copyright © 2021 EVERIS. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
